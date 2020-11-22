44ff15de593a2d490ccdab1b0151bea941f3ef0b
emreisler <emre.isler.9@gmail.com>
2020-11-22 12:29:21 +0300

    ui revised

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication,QMainWindow
 import sys
 from welding import Weld
-from ui import WeldApp
+from ui import Ui_MainWindow
 
+class WeldApp(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(WeldApp, self).__init__(parent)
+        self.setupUi(self)
 
 def main():
     app = QApplication(sys.argv)
