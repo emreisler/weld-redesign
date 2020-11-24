1f649fed52d231409f8fb6f838a3175683c92866
emreisler <emre.isler.9@gmail.com>
2020-11-24 12:08:12 +0300

    graph module is improved

--- graph.py
+++ graph.py
@@ -11,8 +11,8 @@ class Graph(pg.GraphicsWindow):
         self.main_plotter.setLabel('bottom', 'Time', units='s')
         self.main_plotter.showGrid(y= True, x = True, alpha = 1.)
         self.curve_colors = {"measured_voltages" : (0,0,255), "measured_currents" : (0,255,0),"resistance" : (255,0,0),
-                        "TC1" : (25,50,255), "TC2" : (100,150,255),"TC3" : (250,160,120),"TC4" : (150,255,0),"TC5" : (65,75,245),
-                        "TC6" : (254,77,85), "TC7" : (150,255,60),"TC8" : (78,86,0),"TC9" : (125,50,175),"TC10" : (255,255,255)}
+                            "TC1" : (25,50,255), "TC2" : (100,150,255),"TC3" : (250,160,120),"TC4" : (150,255,0),"TC5" : (65,75,245),
+                            "TC6" : (254,77,85), "TC7" : (150,255,60),"TC8" : (78,86,0),"TC9" : (125,50,175),"TC10" : (255,255,255)}
         self.curves = dict()
         for curve in self.curve_colors:
             self.curves[curve] = self.main_plotter.plot(name = curve,pen=pg.mkPen(self.curve_colors[curve],width=1))
@@ -21,10 +21,29 @@ class Graph(pg.GraphicsWindow):
         self.showMaximized()
 
     def draw_aux_curves(self):
+        '''
+        Drawing auxillary curves. Lower and upper melting line horziontally, lower and upper dwell line horziontally, 1st,2nd and 3rd time steps vertically
+        '''
+        raise NotImplementedError
+
+    def clear_graph(self):
+        '''
+        Clearing the polotted curves.
+        '''
         raise NotImplementedError
 
     def draw_cycle(self,**data):
         '''
         Draw the cyle parameters to graph. This function do not use thread because thread is already using by QTimer function which calls this function repeatedly
         '''
-        print(data)
+        print("graph trying to draw")
+
+        try:
+            for (curve_key,curve_item), (arg_key,arg_value) in zip(self.curves.items(), data.items()):
+                if arg_key != "time":
+                    try:
+                        curve_item.setData(data["time"],arg_value)
+                    except Exception as error:
+                        print(f"{arg_key} has no input : ",error)
+        except Exception as error:
+            print("Couldn' t draw : ", error)
--- welding.py
+++ welding.py
@@ -3,11 +3,12 @@ from pymodbus.client.sync import ModbusTcpClient
 from pyqtgraph.Qt import QtCore
 from threading import Thread
 import csv
-from time import perf_counter,sleep
+from time import perf_counter, sleep
 from collections import deque
 from run_cycle import run_cycle
 from make_connections import connect_to_power_supply, connect_to_plc
-
+from random import randint
+import sys
 class Weld:
 
     def __init__(self,ui_object = None, graph_object = None, visa_device_ip = None, visa_device_port = None, modbus_device_ip = None, modbus_device_port = None):
@@ -29,12 +30,13 @@ class Weld:
 
         self.cycle_time_datas = deque()
         self.power_supply_datas = {"voltage" : deque(), "current" : deque(), "resistance" : deque()}
-        self.plc_datas = {"TC1" : deque(), "TC2" : deque(), "TC3" : deque(),"TC4" : deque(),"TC5" : deque(),"TC6" : deque(),"TC7" : deque(),"TC8" : deque(),"TC9" : deque(),"TC10" : deque()}
+        self.plc_datas = {"TC1" : deque(), "TC2" : deque(), "TC3" : deque(),"TC4" : deque(),"TC5" : deque(),"TC6" : deque(),
+                            "TC7" : deque(),"TC8" : deque(),"TC9" : deque(),"TC10" : deque()}
         self.cycle_end = False
         self.connected_to_power_supply = False
         self.connected_to_plc = False
-        self.simulation_mode = False
-
+        self.simulation_mode = True
+        self.cycle_start_time = perf_counter()
 
     def connect(self):
 
@@ -49,6 +51,7 @@ class Weld:
 
     def run_cycle(self):
         try:
+            self.graph_object.clear_graph()
             self.function_threads["run_cycle"].start()
             print("Run cycle thread completed")
             return 0
@@ -59,7 +62,9 @@ class Weld:
     def run_cycle_thread(self):
 
         print( run_cycle(power_supply = None , ui = self.ui_object, voltage1 = self.voltage1,current1 = self.current1,time1 = self.cycle_time1,voltage2 = self.voltage2,
-                  current2 = self.current2 ,time2 = self.cycle_time1,voltage3 = self.voltage3 ,current3 = self.current3,time3 = self.cycle_time1,simulation_mode=self.simulation_mode))
+                  current2 = self.current2 ,time2 = self.cycle_time1,voltage3 = self.voltage3 ,current3 = self.current3,time3 = self.cycle_time1,
+                  simulation_mode=self.simulation_mode))
+        self.cycle_start_time = perf_counter()
 
     def measure_power_supply_cycle(self):
         self.function_threads["measure_power_supply"].start()
@@ -74,9 +79,6 @@ class Weld:
         self.connected_to_power_supply = True if connect_to_power_supply(self.resource_manager,self.ui_object) == 0 else False
 
         self.connected_to_plc = True if connect_to_plc(self.PLC,self.ui_object) == 0 else False
-
-
-
 
         print("Connections are checked")
 
@@ -102,8 +104,6 @@ class Weld:
                 self.resource_manager.close()
             except Exception as error:
                 print("Error in calculating resistance",error)
-
-
 
     def set_parameters(self):
         try:
@@ -136,29 +136,32 @@ class Weld:
         '''
         Take the parameters and push them into data containers of Weld class object.
         '''
-        while not self.cycle_end and connected_to_power_supply:
+        print("Start get data from power supply")
+        if not self.cycle_end and self.connected_to_power_supply:
             voltage_values = self.power_supply.query_ascii_values(':MEASure:VOLTage?')
             measured_voltage = self.voltage_values[0]
-            self.power_supply_data["voltage"].append(measured_voltage)
+            self.power_supply_datas["voltage"].append(measured_voltage)
             self.ui_object.voltage_output_label.setText(f"Voltage {round(measured_voltage, 2)} V")
 
-
-            current_values = self.power_supply_data.query_ascii_values(':MEASure:CURRent?')
+            current_values = self.power_supply_datas.query_ascii_values(':MEASure:CURRent?')
             measured_current = self.current_values[0]
-            self.power_supply_data["current"].append(measured_current)
+            self.power_supply_datas["current"].append(measured_current)
             self.ui_object.current_output_label.setText(f"Current {round(measured_current, 2)} A")
 
+            self.power_supply_datas["resistance"].append(round(measured_voltage / measured_current),2)
 
-            #Resistance will be added
+        if self.simulation_mode:
+            for data in self.power_supply_datas:
+                self.power_supply_datas[data].append(randint(1,200))
 
-
-        raise NotImplementedError
+        print("End get data from power supply")
 
     def measure_plc_cycle_thread(self):
         '''
         Take the parameters and push them into data containers of Weld class object.
         '''
-        while not self.cycle_end and self.connected_to_plc:
+        print("Start get data from plc")
+        if not self.cycle_end and self.connected_to_plc:
             tc_values = self.PLC.read_holding_registers(40, 10, unit=0)
             assert(tc_values.function_code < 0x80)  # test that there is not an error
             i = 0
@@ -169,16 +172,59 @@ class Weld:
                     print(f"TC{i+1} not connected")
                 i +=1
 
+        if self.simulation_mode:
+            for (key,value) in self.plc_datas.items():
+                if key == "TC10":
+                    print("TC10 dont write")
+                    print(value)
+                elif key == "TC7":
+
+                    value.append(rand)
+                    print(value)
+                else:
+                    print(value)
+
+        print("End get data from plc")
+
+    def draw_write(self):
+        """
+        Calls measure power supply and plc functions to update Welder object data containers.
+        And calls draw cycle function of Graph object to draw contained data.
+        """
+        print("preperation to draw")
+        self.measure_power_supply_cycle_thread()
+        self.measure_plc_cycle_thread()
+        self.cycle_time = perf_counter() - self.cycle_start_time
+        self.cycle_time_datas.append(self.cycle_time)
+
+        self.graph_object.draw_cycle(time =self.cycle_time_datas, voltage = self.power_supply_datas["voltage"], current = self.power_supply_datas["current"],
+                                    resistance = self.power_supply_datas["resistance"],
+                                    tc1 = self.plc_datas["TC1"],tc2 = self.plc_datas["TC2"],tc3 = self.plc_datas["TC3"],
+                                    tc4 = self.plc_datas["TC4"],tc5 = self.plc_datas["TC5"],tc6 = self.plc_datas["TC6"],
+                                    tc7 = self.plc_datas["TC7"],tc8 = self.plc_datas["TC8"],tc9 = self.plc_datas["TC9"],
+                                    tc10 = self.plc_datas["TC10"])
+
+        for power_supply_data,plc_data in zip(self.power_supply_datas.values(), self.plc_datas.values()):
+
+            time_data_size = sys.getsizeof(self.cycle_time_datas) / 1048576
+            power_supply_data = sys.getsizeof(self.power_supply_datas) * len(self.power_supply_datas) / 1048576
+            plc_data = sys.getsizeof(self.plc_datas) * len(self.plc_datas) / 1048576
+            total_memory_alloc = time_data_size + power_supply_data + plc_data
+
+            total_data_containing = len(self.cycle_time_datas) * 14
+
+            print(f"Time deque size : {time_data_size} megabytes")
+            print(f"Power_supply data size : {power_supply_data} megabytes")
+            print(f"PLC data size : {plc_data} megabytes")
+            print(f"Total memory allocation for cycle datas : {total_memory_alloc}" )
+            print(f"Total data containing : {total_data_containing}")
+
+    def temperature_panel_writer(self,**data):
         raise NotImplementedError
 
-    def draw(self):
-        print("emre")
-        self.graph_object.draw_cycle(voltage = self.plc_datas["voltage"], current = self.plc_datas["current"] )
-        """
-
-        """
+    def power_supply_panel_writer(self,voltage = 0,current = 0)
         raise NotImplementedError
-        
+
     def emergency_stop_thread(self):
         '''
         Stops power supply in emergency.
