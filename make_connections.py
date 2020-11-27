201c62ec249d3a06e0e6f373ca6f6a2d15c80c05
emreisler <emre.isler.9@gmail.com>
2020-11-27 16:55:58 +0300

    ui is revised

--- make_connections.py
+++ make_connections.py
@@ -3,13 +3,13 @@
 def connect_to_power_supply(resource_manager,ui):
     try:
         power_supply = resource_manager.open_resource(f'TCPIP0::{visa_device_ip}::inst0::INSTR')
-        ui.power_supply_connection.setStyleSheet("background-color: green")
-        ui.power_supply_connection.setText(f"CONNECTED TO \nPOWER SUPPLY")
+        ui.power_supply_connection_button.setStyleSheet("background-color: green")
+        ui.power_supply_connection_button.setText(f"CONNECTED TO \nPOWER SUPPLY")
         return 0
     except Exception as error:
         print("Connection to power supply error : ", error)
-        ui.power_supply_connection.setStyleSheet("background-color: red")
-        ui.power_supply_connection.setText(f"NOT CONNECTED TO \nPOWER SUPPLY...")
+        ui.power_supply_connection_button.setStyleSheet("background-color: red")
+        ui.power_supply_connection_button.setText(f"NOT CONNECTED TO \nPOWER SUPPLY...")
         return 1
 
 def connect_to_plc(plc,ui):
@@ -17,11 +17,11 @@ def connect_to_plc(plc,ui):
         plc.connect()
         tc_values = plc.read_holding_registers(40, 10, unit=0)
         assert(tcValues.function_code < 0x80)
-        ui.temperature_connection.setStyleSheet("background-color: green")
-        ui.temperature_connection.setText(f"CONNECTED TO \nPLC")
+        ui.plc_connection_button.setStyleSheet("background-color: green")
+        ui.plc_connection_button.setText(f"CONNECTED TO \nPLC")
         return 0
     except Exception as error:
-        ui.temperature_connection.setStyleSheet("background-color: red")
-        ui.temperature_connection.setText(f"NOT CONNECTED TO \nPLC...")
+        ui.plc_connection_button.setStyleSheet("background-color: red")
+        ui.plc_connection_button.setText(f"NOT CONNECTED TO \nPLC...")
         print("Connection to PLC error : ", error)
         return 1
